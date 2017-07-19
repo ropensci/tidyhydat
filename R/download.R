827b477a2581a1a49b7386387f2a95bd67119984
Sam Albers <sam.albers@gov.bc.ca>
2017-07-19 15:22:06 -0700

    Better error handling for download_realtime

--- R/download.R
+++ R/download.R
@@ -17,35 +17,36 @@
 #' be used. Currently, if a station does not exist or is not found, no data is returned. Both the province and the station number 
 #' should be specified. 
 #' 
-#' @param STATION_NUMBER Water Survey of Canada station number. No default. Can also take the "ALL" argument at which point you need 
-#' to specify \code{PROV_TERR_STATE_LOC}. 
-#' @param PROV_TERR_STATE_LOC Can be any province. See also for argument options.
+#' @param STATION_NUMBER Water Survey of Canada station number. No default. Can also take the "ALL" argument. 
+#' @param PROV_TERR_STATE_LOC Province, state or territory. See also for argument options.
 #' 
 #' @return A tibble of water flow and level values
-#' @export
 #' 
 #' @seealso 
 #' \code{download_network()}.
 #' 
-#' @note This function is heavily adapted from the RealTimeData function from the HYDAT package. 
+#' @note This function is inspired from the RealTimeData function from the HYDAT package. 
 #' That package can be viewed here: \url{https://github.com/CentreForHydrology/HYDAT}.
 #' Differences between HYDAT::RealTimeData and download_realtime include
 #' \itemize{
 #' \item Column name outputted by download_realtime are identical to corresponding measures in HYDAT
 #' \item Uses readr::read_csv and outputs a tibble
-#' \item download_network is approximately 50 times faster than HYDAT::RealTimeNetwork
+#' \item download_network is approximately 50% times faster than HYDAT::RealTimeNetwork
 #' }
 #' 
 #' @examples
 #' download_realtime(STATION_NUMBER="08MF005", PROV_TERR_STATE_LOC="BC")
 #' 
-#' # To download all stations in Prince George:
+#' # To download all stations in Prince Edward Island:
 #' download_realtime(STATION_NUMBER = "ALL", PROV_TERR_STATE_LOC = "PE")
 #' 
+#' @export
 download_realtime <- function(STATION_NUMBER, PROV_TERR_STATE_LOC) {
   
   if(missing(STATION_NUMBER) | missing(PROV_TERR_STATE_LOC)) 
     stop("STATION_NUMBER or PROV_TERR_STATE_LOC argument is missing. These arguments must match jurisdictions.")
+  
+  ## TODO: HAve a warning message if not internet connection exists
   
   prov = PROV_TERR_STATE_LOC
   
@@ -69,46 +70,61 @@ download_realtime <- function(STATION_NUMBER, PROV_TERR_STATE_LOC) {
                   "FLOW", "FLOW_GRADE", "FLOW_SYMBOL", "FLOW_CODE")
   
   
-  h <- tryCatch(readr::read_csv(infile[1], skip = 1, col_names = colHeaders, col_types = readr::cols(STATION_NUMBER = readr::col_character(),
-                                                                                                date_time = readr::col_datetime(),
-                                                                                                LEVEL = readr::col_double(),
-                                                                                                LEVEL_GRADE = readr::col_character(),
-                                                                                                LEVEL_SYMBOL = readr::col_character(),
-                                                                                                LEVEL_CODE = readr::col_integer(),
-                                                                                                FLOW = readr::col_double(),
-                                                                                                FLOW_GRADE = readr::col_character(),
-                                                                                                FLOW_SYMBOL = readr::col_character(),
-                                                                                                FLOW_CODE = readr::col_integer())
-  ), error = function(c) {
-    c$message <- paste0(STATION_NUMBER_SEL, " cannot be found")
-    stop(c)
-  } )
-
-  
+  h <- tryCatch(
+      readr::read_csv(
+        infile[1],
+        skip = 1,
+        col_names = colHeaders,
+        col_types = readr::cols(
+          STATION_NUMBER = readr::col_character(),
+          date_time = readr::col_datetime(),
+          LEVEL = readr::col_double(),
+          LEVEL_GRADE = readr::col_character(),
+          LEVEL_SYMBOL = readr::col_character(),
+          LEVEL_CODE = readr::col_integer(),
+          FLOW = readr::col_double(),
+          FLOW_GRADE = readr::col_character(),
+          FLOW_SYMBOL = readr::col_character(),
+          FLOW_CODE = readr::col_integer()
+        )
+      ),
+      error = function(c) {
+        c$message <- paste0(STATION_NUMBER_SEL, " cannot be found")
+        stop(c)
+      }
+    )
   
   
   # download daily file
-  d <- tryCatch(readr::read_csv(infile[2], skip = 1, col_names = colHeaders, col_types = readr::cols(STATION_NUMBER = readr::col_character(),
-                                                                                                     date_time = readr::col_datetime(),
-                                                                                                     LEVEL = readr::col_double(),
-                                                                                                     LEVEL_GRADE = readr::col_character(),
-                                                                                                     LEVEL_SYMBOL = readr::col_character(),
-                                                                                                     LEVEL_CODE = readr::col_integer(),
-                                                                                                     FLOW = readr::col_double(),
-                                                                                                     FLOW_GRADE = readr::col_character(),
-                                                                                                     FLOW_SYMBOL = readr::col_character(),
-                                                                                                     FLOW_CODE = readr::col_integer())
-  ), error = function(c) {
-    c$message <- paste0(STATION_NUMBER_SEL, " cannot be found")
-    stop(c)
-  } )
+  d <- tryCatch(
+      readr::read_csv(
+        infile[2],
+        skip = 1,
+        col_names = colHeaders,
+        col_types = readr::cols(
+          STATION_NUMBER = readr::col_character(),
+          date_time = readr::col_datetime(),
+          LEVEL = readr::col_double(),
+          LEVEL_GRADE = readr::col_character(),
+          LEVEL_SYMBOL = readr::col_character(),
+          LEVEL_CODE = readr::col_integer(),
+          FLOW = readr::col_double(),
+          FLOW_GRADE = readr::col_character(),
+          FLOW_SYMBOL = readr::col_character(),
+          FLOW_CODE = readr::col_integer()
+        )
+      ),
+      error = function(c) {
+        c$message <- paste0(STATION_NUMBER_SEL, " cannot be found")
+        stop(c)
+      }
+    )
 
 
   # now merge the hourly + daily (hourly data overwrites daily where dates are the same)
   p <- which(d$date_time < min(h$date_time))
   output <- rbind(d[p,], h)
  
-  
 
   
   output_c <- dplyr::bind_rows(output, output_c)
@@ -139,9 +155,19 @@ download_network <- function(PROV_TERR_STATE_LOC){
   ## Need to implement a search by station
   #try((if(hasArg(PROV_TERR_STATE_LOC_SEL) == FALSE) stop("Stopppppte")))
   
-  net_tibble <- readr::read_csv("http://dd.weather.gc.ca/hydrometric/doc/hydrometric_StationList.csv", skip = 1,
-                                col_names = c("STATION_NUMBER", "STATION_NAME", "LATITUDE", "LONGITUDE", 
-                                              "PROV_TERR_STATE_LOC", "TIMEZONE"), col_types = readr::cols())
+  net_tibble <- readr::read_csv(
+      "http://dd.weather.gc.ca/hydrometric/doc/hydrometric_StationList.csv",
+      skip = 1,
+      col_names = c(
+        "STATION_NUMBER",
+        "STATION_NAME",
+        "LATITUDE",
+        "LONGITUDE",
+        "PROV_TERR_STATE_LOC",
+        "TIMEZONE"
+      ),
+      col_types = readr::cols()
+    )
   
   if((prov == "ALL")[1]){
     return(net_tibble)
