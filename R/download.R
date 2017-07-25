b2dde7ad8ffad92ea670c36fb8638a8e7b8362b0
Sam Albers <sam.albers@gov.bc.ca>
2017-07-25 15:42:17 -0700

    download_ws and get_ws_token function and docs

--- R/download.R
+++ R/download.R
@@ -177,6 +177,118 @@ download_network <- function(PROV_TERR_STATE_LOC){
   return(net_tibble)
 }
 
+#' @title Request a token from the Environment and Climate Change Canada  
+#' @param username Supplied by ECCC
+#' @param password Supplied by ECCC
+#' @description Request a token from the ECCC webservice using the POST method. This token expires after 10 minutes. 
+#' You can only have 5 tokens out at once. 
+#' 
+#' 
+#' @return The token as a string that should be supplied the \code{download_ws_realtime} function.
+#' 
+#' @export
+
+get_ws_token <- function(username, password){
+  login <- list(
+    username = username,
+    password = password
+  )
+  r = httr::POST("https://wateroffice.ec.gc.ca/services/auth", body = login)
+  
+  ## If the POST request was not a successful, print out why.
+  ## Possibly could provide errors as per Webservice guidelines
+  httr::stop_for_status(r)
+  
+  message(paste0("This token will expire at ",format(Sys.time() + 10*60, "%H:%M:%S")))
+  
+  ## Extract token from POST
+  token = httr::content(r, "text", encoding = "ISO-8859-1")
+  
+  return(token)
+  
+  
+}
+
+#' @title Download data from the ECCC web service
+#' @description Function to actually retrieve data from ECCC webservice. Before using this function, 
+#' a token from \code{get_ws_token()} is needed.
+#' @param STATION_NUMBER Water Survey of Canada station number.
+#' @param parameters parameter ID
+#' @param start_date Need to be in YYYY-MM-DD
+#' @param end_date Need to be in YYYY-MM-DD
+#' @param token generate by \code{get_ws_token()}
+#' @export
+
+
+download_ws <- function(STATION_NUMBER, parameters, start_date, end_date, token){
+  if(length(STATION_NUMBER) >= 300){
+    stop("Only 300 stations are supported for one request. If more stations are required, 
+         a separate request should be issued to include the excess stations. This second request will 
+         require an additional token.")
+  }
+  
+  ## Check date is in the right format
+  if(is.na(as.Date(start_date, format = "%Y-%m-%d"))  | is.na(as.Date(end_date, format = "%Y-%m-%d")) ){
+    stop("Invalid date format. Dates need to be in YYYY-MM-DD format")
+  }
+  
+  if(as.Date(end_date) - as.Date(start_date) > 60){
+    stop("The time period of data being requested should not exceed 2 months. 
+         If more data is required, then a separate request should be issued to include a different time period.")
+  }
+  ## English parameter names
+  
+  ## Is it a valid parameter name?
+  
+  ## Is it a valid Station name? 
+  
+  ## Username and password
+  
+  
+  
+  ##Build link for GET
+  baseurl = "https://wateroffice.ec.gc.ca/services/real_time_data/csv/inline?"
+  station_string = paste0("stations[]=", STATION_NUMBER, collapse = "&")
+  parameters_string = paste0("parameters[]=", parameters, collapse = "&")
+  date_string = paste0("start_date=", start_date, "%2000:00:00&end_date=", end_date,"%2023:59:59")
+  token_string = paste0("token=",token)
+  
+  ## paste them all together
+  url_for_GET = paste0(baseurl,
+                       station_string,"&",
+                       parameters_string,"&",
+                       date_string,"&",
+                       token_string)
+  
+  ## Get data
+  get_ws = httr::GET(url_for_GET)
+  
+  ## Check the GET status
+  httr::stop_for_status(get_ws)
+  
+  if (httr::headers(get_ws)$`content-type` != "text/csv; charset=utf-8"){
+    stop("GET response is not a csv file")
+  }
+  
+  ## Turn it into a tibble
+  csv_df <- httr::content(get_ws)
+  
+  ## What stations were missed?
+  differ = setdiff(unique(STATION_NUMBER), unique(csv_df$ID))
+  if( length(differ) !=0 ){
+    message("The following station(s) were not retrieved: ", paste0(differ, sep = " "))
+    message("See ?download_ws_realtime for possible reasons why.")
+  } else{
+    message("All station successfully retrieved")
+  }
+  
+  ##Return it
+  return(csv_df)
+  
+  ## Need to output a warning to see if any stations weren't retrieved
+  }
+
+
 
 #download_hydat <- function() {
 #  url <- 'http://collaboration.cmc.ec.gc.ca/cmc/hydrometrics/www/'
