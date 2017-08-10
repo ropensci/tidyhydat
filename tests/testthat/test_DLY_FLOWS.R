da7326cc1f70618450c1db137c30ac389b847da6
Sam Albers <sam.albers@gov.bc.ca>
2017-08-10 15:14:19 -0700

    added some units tests

--- tests/testthat/test_DLY_FLOWS.R
+++ tests/testthat/test_DLY_FLOWS.R
@@ -7,7 +7,6 @@ test_that("DLY_FLOWS downloads one station", {
                               PROV_TERR_STATE_LOC = "BC", 
                               hydat_path = "H:/Hydat.sqlite3")$STATION_NUMBER),
                     stns)
-  
 })
 
 
@@ -17,5 +16,20 @@ test_that("DLY_FLOWS downloads multiple stations", {
                                      PROV_TERR_STATE_LOC = "BC", 
                                      hydat_path = "H:/Hydat.sqlite3")$STATION_NUMBER)),
                     length(stns))
+})
+
+
+test_that("DLY_FLOWS respects Date specification", {
+  date_vector = c("2013-01-01","2014-01-01")
+  temp_df <- DLY_FLOWS(STATION_NUMBER = "08MF005", PROV_TERR_STATE_LOC = "BC", hydat_path = "H:/Hydat.sqlite3",
+            start_date = date_vector[1] ,
+            end_date = date_vector[2])
+  expect_identical(c(min(temp_df$Date), max(temp_df$Date)), as.Date(date_vector) )
   
+})
+
+test_that("DLY_FLOWS correctly parses leaps year",{
+  DLY_FLOWS(STATION_NUMBER = c("08HB074", "10BE009", "08FB005"), PROV_TERR_STATE_LOC = "BC", hydat_path = "H:/Hydat.sqlite3",
+            start_date = "1988-02-29" ,
+            end_date = "1988-02-29")
 })
--- tests/testthat/test_download_realtime.R
+++ tests/testthat/test_download_realtime.R
@@ -1,5 +1,8 @@
-context("Testing download_realtime")
+context("Testing download_realtime2")
 
-test_that("download_realtime downloads data to correct specs", {
-  expect_identical(1,1)
+test_that("download_realtime2 returns the correct data header", {
+  
+  expect_identical(colnames(download_realtime2(STATION_NUMBER = "08MF005", PROV_TERR_STATE_LOC = "BC")), 
+                   c("STATION_NUMBER", "Date", "LEVEL", "LEVEL_GRADE", "LEVEL_SYMBOL", 
+                     "LEVEL_CODE", "FLOW", "FLOW_GRADE", "FLOW_SYMBOL", "FLOW_CODE"))
 })
