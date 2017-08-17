72c75bad30d67306c465e1cd403e26ecf42b47f3
Sam Albers <sam.albers@gov.bc.ca>
2017-08-17 09:44:04 -0700

    Update test to include download_realtime

--- tests/testthat/test_download_realtime.R
+++ tests/testthat/test_download_realtime.R
@@ -1,7 +1,22 @@
-context("Testing download_realtime2")
+context("Testing download_realtime functions")
+
+test_that("download_realtime2 returns the correct data header", {
+  token_out <- get_ws_token(username = Sys.getenv("WS_USRNM"), password = Sys.getenv("WS_PWD"))
+  
+  ws_test <- download_realtime(STATION_NUMBER = "08MF005",
+                               parameters = c(46), ## Water level and temperature
+                               start_date = Sys.Date(),
+                               end_date = Sys.Date(),
+                               token = token_out)
+  
+  expect_identical(colnames(ws_test), 
+                   c("STATION_NUMBER", "Date", "Name_En", "Value", "Unit", "Grade", 
+                     "Symbol", "Approval", "Parameter", "Code"))
+})
+
 
 test_that("download_realtime2 returns the correct data header", {
   
   expect_identical(colnames(download_realtime2(STATION_NUMBER = "08MF005", PROV_TERR_STATE_LOC = "BC")), 
-                   c("STATION_NUMBER", "Date", "Paramter","Value","Grade", "Symbol","Code"))
+                   c("STATION_NUMBER", "Date", "Parameter","Value","Grade", "Symbol","Code"))
 })
