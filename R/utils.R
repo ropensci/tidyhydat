e2013037cb7537b7505102b3e760b548d23075f5
Sam Albers <sam.albers@gov.bc.ca>
2017-08-23 15:58:45 -0700

    provide more lookup tables and internal data to interpret status codes from hydat; closes #6

--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,73 @@ search_name = function(search_term){
   }
   
 }
+
+#' @title AGENCY_LIST function
+#' 
+#' @description AGENCY_LIST – AGENCY look-up Table
+#' @param hydat_path Directory to the hydat database. Can be set as "Hydat.sqlite3" which will look for Hydat in the working directory 
+#' 
+#' @return A tibble of agencies
+#' 
+#' @export
+#' 
+AGENCY_LIST = function(hydat_path){
+  
+  ## Read on database
+  hydat_con <- DBI::dbConnect(RSQLite::SQLite(), hydat_path)
+  
+  agency_list = dplyr::tbl(hydat_con, "AGENCY_LIST") %>%
+    collect()
+  
+  DBI::dbDisconnect(hydat_con)
+  
+  return(agency_list)
+  
+}
+
+
+#' @title REGIONAL_OFFICE_LIST function
+#' 
+#' @description REGIONAL_OFFICE_LIST – OFFICE look-up Table
+#' @param hydat_path Directory to the hydat database. Can be set as "Hydat.sqlite3" which will look for Hydat in the working directory  
+#' @return A tibble of offices
+#' 
+#' @export
+#' 
+REGIONAL_OFFICE_LIST = function(hydat_path){
+  
+  ## Read on database
+  hydat_con <- DBI::dbConnect(RSQLite::SQLite(), hydat_path)
+  
+  regional_office_list = dplyr::tbl(hydat_con, "REGIONAL_OFFICE_LIST") %>%
+    collect()
+  
+  DBI::dbDisconnect(hydat_con)
+  
+  return(regional_office_list)
+  
+}
+
+#' @title DATUM_LIST function
+#' 
+#' @description DATUM_LIST – DATUM look-up Table
+#' @param hydat_path Directory to the hydat database. Can be set as "Hydat.sqlite3" which will look for Hydat in the working directory 
+#' 
+#' @return A tibble of DATUMS
+#' 
+#' @export
+#' 
+DATUM_LIST = function(hydat_path){
+  
+  ## Read on database
+  hydat_con <- DBI::dbConnect(RSQLite::SQLite(), hydat_path)
+  
+  datum_list = dplyr::tbl(hydat_con, "DATUM_LIST") %>%
+    collect()
+  
+  DBI::dbDisconnect(hydat_con)
+  
+  return(datum_list)
+  
+}
+
