f6f54b589d80f3234ea6bf137debfd0425607ab4
Sam Albers <sam.albers@gov.bc.ca>
2017-08-29 16:28:29 -0700

    Added download_hydat Closes #3; multi prov for dd realtime; better error messages closes #16

--- tests/testthat/test_download_realtime.R
+++ tests/testthat/test_download_realtime.R
@@ -16,7 +16,19 @@ context("Testing download_realtime functions")
 #
 
 test_that("download_realtime_dd returns the correct data header", {
-  
   expect_identical(colnames(download_realtime_dd(STATION_NUMBER = "08MF005", PROV_TERR_STATE_LOC = "BC")), 
-                   c("STATION_NUMBER", "Date", "Parameter","Value","Grade", "Symbol","Code"))
+                   c("STATION_NUMBER","PROV_TERR_STATE_LOC","Date", "Parameter","Value","Grade", "Symbol","Code"))
+})
+
+test_that("download_realtime_dd can download stations from multiple provinces using PROV_TERR_STATE_LOC",{
+  download_realtime_dd(PROV_TERR_STATE_LOC = c("QC","PE"))
+})
+
+
+test_that("download_realtime_dd can download stations from multiple provinces using STATION_NUMBER",{
+  expect_error(download_realtime_dd(STATION_NUMBER = c("01CD005","08MF005")), regexp = NA)
+})
+
+test_that("When STATION_NUMBER is ALL there is an error",{
+  expect_error(download_realtime_dd(STATION_NUMBER = "ALL"))
 })
