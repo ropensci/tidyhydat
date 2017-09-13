a68f7635a1b94e49ae4eb0c028ca3446bd8f41bc
Sam Albers <sam.albers@gov.bc.ca>
2017-09-13 11:15:57 -0700

    test db now includes all tables from HYDAT

--- data-raw/HYDAT_internal_data/tinyhydat_proc.R
+++ data-raw/HYDAT_internal_data/tinyhydat_proc.R
@@ -19,12 +19,16 @@ library(dbplyr)
 ## Create a subset of the data
 hydat_con <- DBI::dbConnect(RSQLite::SQLite(), Sys.getenv("hydat"))
 
+all_tables <- DBI::dbListTables(hydat_con)
+
 #table_vector <- DBI::dbListTables(hydat_con)
 # Don't need all the tables. I will export what I need for testing
 table_vector <- c("ANNUAL_INSTANT_PEAKS", "ANNUAL_STATISTICS", 
                   "DLY_FLOWS", "DLY_LEVELS", "SED_DLY_LOADS", "SED_DLY_SUSCON", "SED_SAMPLES", "SED_SAMPLES_PSD", 
-                  "STATIONS","STN_REGULATION")
+                  "STATIONS","STN_REGULATION","STN_REMARKS", "STN_DATUM_CONVERSION", "STN_DATA_RANGE","STN_DATA_COLLECTION",
+                  "STN_OPERATION_SCHEDULE","STN_DATUM_UNRELATED")
 
+## List of tables with STATION_NUMBER INFORMATION
 list_of_small_tables <- table_vector %>%
   map(~tbl(src = hydat_con, .) %>%
         filter(STATION_NUMBER %in% c("08MF005","08NM083","08NE102","08AA003",
@@ -32,7 +36,15 @@ list_of_small_tables <- table_vector %>%
         collect()
       )
 
-## Need this table for SED_SAMPLES_PSD
+## All tables without STATION_NUMBER
+no_stn_table_vector <- all_tables[!all_tables %in% table_vector]
+
+list_of_no_stn_tables <- no_stn_table_vector %>%
+  map(~tbl(src = hydat_con, .) %>%
+        head(50) %>%
+        collect()
+  )
+
 SED_DATA_TYPES <- dplyr::tbl(hydat_con, "SED_DATA_TYPES") %>% collect()
 
 DBI::dbDisconnect(hydat_con)
@@ -46,15 +58,24 @@ con <- DBI::dbConnect(RSQLite::SQLite(), db_path)
 
 ## Do this in a loop - uncertain how to do it purrr.
 ## Because this isn't a regularly run item I'll leave it as is.
+## Loops for Stns
 for (i in 1:length(table_vector)) {
   DBI::dbWriteTable(con, table_vector[i], list_of_small_tables[[i]], overwrite=TRUE)
 }
-DBI::dbWriteTable(con, "SED_DATA_TYPES", SED_DATA_TYPES, overwrite=TRUE)
+
+## Tables without station info
+for (i in 1:length(no_stn_table_vector)) {
+  DBI::dbWriteTable(con, no_stn_table_vector[i], list_of_no_stn_tables[[i]], overwrite=TRUE)
+}
+
+
+#DBI::dbWriteTable(con, "SED_DATA_TYPES", SED_DATA_TYPES, overwrite=TRUE)
 
 DBI::dbDisconnect(con)
 
 ## Check to make sure the tables that I want are in the db
 testdb <- DBI::dbConnect(RSQLite::SQLite(), db_path)
 
-DBI::dbListTables(testdb) == table_vector
+## Check to make sure all the tables from HYDAT are in the test db
+all(DBI::dbListTables(testdb) %in% all_tables) == TRUE
 
