2ca1ad2363eef0c92cf86f90471ea2840440d32b
Sam Albers <sam.albers@gov.bc.ca>
2017-10-11 08:49:47 -0700

    fixed get_ws_token example

--- R/download.R
+++ R/download.R
@@ -266,7 +266,7 @@ realtime_network_meta <- function(PROV_TERR_STATE_LOC = NULL) {
 #'
 #' @return The token as a string that should be supplied the \code{download_ws_realtime} function.
 #' 
-#' @examples \donttest{See ?download_realtime_ws}
+#' @example \donttest{See ?download_realtime_ws}
 #' 
 #' @family realtime functions
 #' @export
