636b90d0a65c83c63ba107fe5d7e533c0c3ad694
Sam Albers <sam.albers@gov.bc.ca>
2018-01-04 13:32:19 -0800

    realtime_plot now acccepts multiple stations

--- R/realtime_plot.R
+++ R/realtime_plot.R
@@ -13,16 +13,21 @@
 #' Convenience function to plot realtime data
 #' 
 #' This is a quick and easy way to visualize a single station using base R graphics. 
-#' More complicated plotting needs should consider using \code{ggplot2}. 
+#' More complicated plotting needs should consider using \code{ggplot2}. Inputting more 
+#' 5 stations will result in very busy plots and longer load time. 
 #' 
-#' @inheritParams hy_stations
+#' @param station_number A (or several) seven digit Water Survey of Canada station number. 
 #' @param Parameter Parameter of interest. Either "Flow" or "Level".
 #' 
 #' @return A plot of recent realtime values
 #' 
 #' @examples 
 #' \dontrun{
+#' ## One station
 #' realtime_plot("08MF005")
+#' 
+#' ## Multiple stations
+#' realtime_plot(c("07EC002","01AD003"))
 #' }
 #' 
 #' @export
@@ -31,23 +36,37 @@ realtime_plot <- function(station_number = NULL, Parameter = c("Flow","Level")){
   
   Parameter = match.arg(Parameter)
   
-  if(length(station_number) > 1L) stop("realtime_plot only accepts one station number")
+  #if(length(station_number) > 1L) stop("realtime_plot only accepts one station number")
   
   rldf <- realtime_dd(station_number)
   
-  if(is.null(rldf)) stop("Station not present in the datamart")
+  if(is.null(rldf)) stop("Station(s) not present in the datamart")
   
   rldf <- rldf[rldf$Parameter == Parameter,]
+  rldf$STATION_NUMBER <- factor(rldf$STATION_NUMBER)
   
-  stn_name <- search_stn_number(station_number)$STATION_NAME
+  #stn_name <- search_stn_number(station_number)$STATION_NAME
   
   y_axis <- ifelse(Parameter == "Flow", "Discharge (m^3/s)", "Level (m)")
   
-  graphics::plot(x = rldf$Date, y = rldf$Value, 
-       type = "l", 
-       col = "blue",
-       main=paste0(stn_name, " - ", station_number, "\n Parameter of interest: ", Parameter),
-       xlab="Date", ylab=paste0(y_axis))
+  
+  ## Set the palette
+  #palette(rainbow(length(unique(rldf$STATION_NUMBER))))
+  
+  graphics::plot(Value ~ Date,
+                 data = rldf,
+                 col = STATION_NUMBER,
+                 main="Realtime Water Survey of Canada Gauges",
+                 xlab="Date", 
+                 ylab=paste0(y_axis),
+                 bty= "L",
+                 pch = 20, cex = 1)
+
+  graphics::legend(x = "topright",
+         legend = unique(rldf$STATION_NUMBER), 
+         fill = unique(rldf$STATION_NUMBER),
+         bty = "n",
+         cex = 0.75)
   
   
 }
