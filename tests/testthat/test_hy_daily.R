ca8ba2089d0c94f0e1e5208038818fe0c4ac586b
Sam Albers <sam.albers@gov.bc.ca>
2018-02-14 13:53:21 -0800

    hy_daily test now uses test database

--- tests/testthat/test_hy_daily.R
+++ tests/testthat/test_hy_daily.R
@@ -1,13 +1,14 @@
 context("Testing hy_daily")
 
 test_that("hy_daily accepts a level only station argument", {
+  #skip_on_cran()
   stns <- "08NM083"
- expect_silent(hy_daily(stns))
+ expect_silent(hy_daily(stns,hydat_path = hy_test_db()))
 })
 
 test_that("hy_daily accepts multiple station arguments", {
   stns <- c("08NM083","08MF005")
-  expect_silent(multi_stn_data <- hy_daily(stns))
+  expect_silent(multi_stn_data <- hy_daily(stns,hydat_path = hy_test_db()))
   expect_identical(length(stns), length(unique(multi_stn_data$STATION_NUMBER)))
 })
 
