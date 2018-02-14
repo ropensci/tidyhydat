cf55c76a663132ba890f80a077f3b5dfdf92df18
Sam Albers <sam.albers@gov.bc.ca>
2018-02-14 14:40:15 -0800

    skipping some hy_daily tests

--- tests/testthat/test_hy_daily.R
+++ tests/testthat/test_hy_daily.R
@@ -9,6 +9,9 @@ test_that("hy_daily accepts a level only station argument", {
 test_that("hy_daily accepts multiple station arguments", {
   stns <- c("08NM083","08MF005")
   expect_silent(multi_stn_data <- hy_daily(stns,hydat_path = hy_test_db()))
+  
+  skip_on_cran()
+  skip_on_travis()
   expect_identical(length(stns), length(unique(multi_stn_data$STATION_NUMBER)))
 })
 
