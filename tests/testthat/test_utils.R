784aad9d10e4671045818e83fec506885b4415d0
rywhale <rdgwhaley@gmail.com>
2018-06-22 04:26:52 -0700

    Added unit test for network_check function. Swapped network_check from checking base_url to whatever url is fed into it.

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -24,3 +24,15 @@ test_that("hy_version returns a dataframe and works",{
   expect_silent(hy_version(hydat_path = hy_test_db()))
 })
 
+test_that("downloading hydat fails behind a proxy server with informative error message",{
+  httr::set_config(httr::use_proxy(url = "http://google.com", port = 1234), override = TRUE)
+  base_url <-
+    "http://collaboration.cmc.ec.gc.ca/cmc/hydrometrics/www/"
+  expect_error(network_check(base_url), messsage = paste0("Error: Could not connect to HYDAT source. 
+                                                    Check your connection settings.
+                                                    Try downloading HYDAT_sqlite3 from this url: 
+                                                    [http://collaboration.cmc.ec.gc.ca/cmc/hydrometrics/www/]
+                                                    and unzipping the saved file to this directory:",
+                                                          hy_dir()))
+  httr::reset_config()
+})
