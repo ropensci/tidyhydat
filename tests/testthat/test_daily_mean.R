8aeaa30b97e999e6b7b34672ed5542851e2b0d78
Sam Albers <sam.albers@gov.bc.ca>
2018-06-26 11:02:27 -0700

    skip on cran daily mean test

--- tests/testthat/test_daily_mean.R
+++ tests/testthat/test_daily_mean.R
@@ -1,6 +1,7 @@
 context("Testing daily_mean()")
 
 test_that("daily_mean takes a range of values for yesterday and reduces it to one value per parameter",{
+  skip_on_cran()
   df <- realtime_dd("08MF005")
   df_yesterday <- df[as.Date(df$Date) == Sys.Date() - 1,]
     expect_equal(nrow(realtime_daily_mean(df_yesterday, na.rm = FALSE)), 2)
