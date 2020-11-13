f11673725b4967bae13b56bfed5ee0e50ec4668b
Sam Albers <sam.albers@gov.bc.ca>
2020-11-13 10:56:08 -0800

    look for missing tables as well

--- R/download.R
+++ R/download.R
@@ -123,13 +123,17 @@ download_hydat <- function(dl_hydat_here = NULL) {
 }
 
 hy_check <- function() {
-  green_message("Checking all HYDAT tables for data")
   con <- hy_src()
   on.exit(hy_src_disconnect(con))
   
   have_tbls <- dplyr::src_tbls(con)
   
-  x <- hy_expected_tbls()
+  tbl_diff <- setdiff(hy_expected_tbls(), have_tbls)
+  if (!rlang::is_empty(tbl_diff)) {
+    red_message("The following tables are missing from HYDAT")
+    red_message(paste0(tbl_diff, "\n"))
+  }
+  
   
   invisible(lapply(have_tbls, function(x) {
     tbl_rows <- dplyr::tbl(con, x) %>% 
