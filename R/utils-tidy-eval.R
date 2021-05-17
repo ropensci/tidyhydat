5d39549687fbc5430b7a005f83de9c19e5d4154d
Sam Albers <sam.albers@gov.bc.ca>
2021-05-17 12:09:28 -0700

    remove dead link

--- R/utils-tidy-eval.R
+++ R/utils-tidy-eval.R
@@ -3,8 +3,6 @@
 #' These functions provide tidy eval-compatible ways to capture
 #' symbols (`sym()`, `syms()`, `ensym()`), expressions (`expr()`,
 #' `exprs()`, `enexpr()`), and quosures (`quo()`, `quos()`, `enquo()`).
-#' To learn more about tidy eval and how to use these tools, read
-#' <http://rlang.tidyverse.org/articles/tidy-evaluation.html>
 #'
 #' @name tidyeval
 #' @keywords internal
