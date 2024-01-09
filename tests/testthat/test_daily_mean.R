bd7a76019c7697f55dc2d6befdd157980440fe9a
Sam Albers <sam.albers@gmail.com>
2024-01-08 20:31:03 -0800

    fix to be utc

--- tests/testthat/test_daily_mean.R
+++ tests/testthat/test_daily_mean.R
@@ -1,6 +1,8 @@
 test_that("daily_mean takes a range of values for yesterday and reduces it to one value per parameter", {
   skip_on_cran()
+
+  date_utc <- as.Date(as.POSIXct(Sys.time(), tz = "UTC"))
   df <- realtime_dd("08MF005")
-  df_yesterday <- df[as.Date(df$Date) == Sys.Date() - 1, ]
+  df_yesterday <- df[as.Date(df$Date) == date_utc - 1, ]
   expect_equal(nrow(realtime_daily_mean(df_yesterday, na.rm = FALSE)), 2)
 })
