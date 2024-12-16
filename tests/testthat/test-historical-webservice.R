dd4d92730ac988820d14e2b76cb92582dbb03ee0
Sam Albers <sam.albers@gmail.com>
2024-12-15 21:05:49 -0800

    add levels to test

--- tests/testthat/test-historical-webservice.R
+++ tests/testthat/test-historical-webservice.R
@@ -1,4 +1,4 @@
-test_that("historical_ws returns the correct data header", {
+test_that("ws_daily_flows returns the correct data header", {
   skip_on_cran()
 
   ws_test <- ws_daily_flows(station_number = "08MF005")
@@ -9,20 +9,39 @@ test_that("historical_ws returns the correct data header", {
       "STATION_NUMBER", "Date", "Parameter", "Value", "Symbol"
     )
   )
-
-  ## Turned #42 into a test
   expect_true(is.numeric(ws_test$Value))
 })
 
 
-test_that("historical_ws is empty is a nearish date", {
+test_that("ws_daily_flows is empty with a nearish date", {
   skip_on_cran()
 
   expect_error(ws_daily_flows(
     station_number = "08MF005",
     start_date = Sys.Date() - 2
   ), "No data exists for this station query during the period chosen")
-  
+})
+
+test_that("ws_daily_levels returns the correct data header", {
+  skip_on_cran()
+
+  ws_test <- ws_daily_levels(station_number = "08MF005")
+
+  expect_identical(
+    colnames(ws_test),
+    c(
+      "STATION_NUMBER", "Date", "Parameter", "Value", "Symbol"
+    )
+  )
+  expect_true(is.numeric(ws_test$Value))
 })
 
 
+test_that("ws_daily_levels is empty with a nearish date", {
+  skip_on_cran()
+
+  expect_error(ws_daily_levels(
+    station_number = "08MF005",
+    start_date = Sys.Date() - 2
+  ), "No data exists for this station query during the period chosen")
+})
