2bd8f8951d96e3a5f3756d9d828212f43321e8ad
Sam Albers <sam.albers@gmail.com>
2025-10-22 15:13:01 -0700

    FIX: base url change (#228)
    * change base url
    
    * fix prov realtime
    
    * update internal data

--- R/utils-realtime.R
+++ R/utils-realtime.R
@@ -49,7 +49,7 @@ single_realtime_station <- function(station_number) {
     }
   }
 
-  base_url <- "https://dd.weather.gc.ca/hydrometric"
+  base_url <- base_url_datamart()
 
   # build URL
   type <- c("hourly", "daily")
@@ -126,8 +126,15 @@ single_realtime_station <- function(station_number) {
 }
 
 all_realtime_station <- function(PROV) {
-  base_url <- "https://dd.weather.gc.ca/hydrometric/csv/"
-  prov_url <- paste0(base_url, PROV, "/daily/", PROV, "_daily_hydrometric.csv")
+  base_url <- base_url_datamart()
+  prov_url <- paste0(
+    base_url,
+    "csv/",
+    PROV,
+    "/daily/",
+    PROV,
+    "_daily_hydrometric.csv"
+  )
 
   res <- realtime_parser(prov_url)
 
